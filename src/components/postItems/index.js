230e70e2823b438ec2bbd255a436f0efc6eb801e
Jaspier <dungoallen349@gmail.com>
2022-09-02 21:13:20 +0100

    Created the post component

---
+++ src/components/postItems/index.js
@@ -0,0 +1,32 @@
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  Platform,
+} from "react-native";
+import styles from "./styles";
+
+const PostItems = () => {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" backgroundColor="#90EE90" />
+      <View style={styles.postWrap}>
+        <Image
+          source={{ uri: "https://picsum.photos/200/300" }}
+          style={styles.postImage}
+        ></Image>
+        <View style={styles.postContentWrap}>
+          <View>
+            <Text style={styles.postTitle}>This is title for rent.</Text>
+            <Text style={styles.postPlace}>Belfast</Text>
+          </View>
+          <Text style={styles.postValue}>£100 / day</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+export default PostItems;
