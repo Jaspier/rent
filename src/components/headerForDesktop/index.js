47a898b1db7f29f3d31a3ec4d7d87bb81aeefa22
Allen Dungo <allendungo@Allens-MBP.broadband>
2022-09-08 21:20:32 +0100

    Designing header for desktop version (right side completed)

--- src/components/headerForDesktop/index.js
+++ src/components/headerForDesktop/index.js
@@ -1,8 +1,15 @@
-import { Text, View, TextInput, Dimensions } from "react-native";
+import {
+	Text,
+	TextInput,
+	View,
+	Image,
+	Dimensions,
+	Pressable,
+} from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { MaterialIcons } from "@expo/vector-icons";
 import { colors } from "../../modal/color";
-import styles from "./styles.js";
+import styles from "./styles";
 
 const HeaderForDesktop = () => {
 	const windowWidth = Number(Dimensions.get("window").width);
@@ -12,31 +19,110 @@ const HeaderForDesktop = () => {
 			<View
 				style={[
 					styles.headerWrap,
-					{ display: windowWidth > 800 ? "flex" : "none" },
+					{
+						display: windowWidth > 800 ? "flex" : "none",
+						height: 80,
+						backgroundColor: colors.primary,
+						width: "100%",
+						justifyContent: "center",
+						alignItems: "center",
+					},
 				]}
 			>
-				<View style={styles.searchByTextWrap}>
-					<Feather name="search" size={24} color={colors.black} />
-					<TextInput
-						placeholder="Search in Rent.com"
-						style={styles.searchPlaceholder}
-						multiline={false}
-					/>
-				</View>
-				<View style={styles.locationCatSearchWrap}>
-					<View style={styles.locationSearchWrap}>
-						<MaterialIcons name="location-on" size={20} color={colors.black} />
-						<Text>location</Text>
-						<Text style={styles.locationSearchText}>Belfast</Text>
+				<View
+					style={{
+						width: "80%",
+						flexDirection: "row",
+						justifyContent: "space-between",
+					}}
+				>
+					<View
+						style={{
+							flexDirection: "row",
+							justifyContent: "flex-start",
+							alignItems: "center",
+						}}
+					>
+						<View
+							style={{
+								height: 60,
+								width: 60,
+								justifyContent: "center",
+								alignItems: "flex-start",
+							}}
+						>
+							<Image
+								resizeMode="contain"
+								source={require("../../../assets/logo.png")}
+								style={{ width: "100%", height: "100%" }}
+							/>
+						</View>
+						<View
+							style={{
+								marginLeft: 15,
+								flexDirection: "row",
+								justifyContent: "center",
+								alignItems: "center",
+							}}
+						>
+							<TextInput
+								multiline={false}
+								placeholder="Search Rent.com"
+								style={{
+									backgroundColor: colors.white,
+									height: 40,
+									width: 250,
+									padding: 5,
+									borderBottomLeftRadius: 5,
+									borderTopLeftRadius: 5,
+									borderWidth: 2,
+									borderColor: colors.secondary,
+								}}
+							/>
+							<Pressable>
+								<Text
+									style={{
+										backgroundColor: colors.primary,
+										height: 40,
+										padding: 8.5,
+										borderBottomRightRadius: 5,
+										borderTopRightRadius: 5,
+										borderWidth: 2,
+										borderColor: colors.secondary,
+										marginLeft: -1.8,
+										fontWeight: "bold",
+										color: colors.secondary,
+									}}
+								>
+									Search
+								</Text>
+							</Pressable>
+						</View>
+						<View
+							style={{
+								flexDirection: "row",
+								paddingVertical: 15,
+								paddingHorizontal: 10,
+								justifyContent: "center",
+								alignItems: "center",
+							}}
+						>
+							<MaterialIcons
+								name="location-on"
+								size={24}
+								color={colors.secondary}
+							/>
+							<Text style={{ color: colors.secondary }}>
+								Location{" "}
+								<Text style={{ fontWeight: "bold", color: colors.secondary }}>
+									Belfast
+								</Text>
+							</Text>
+						</View>
 					</View>
-					<View style={styles.catSearchWrap}>
-						<MaterialIcons
-							name="settings-input-component"
-							size={20}
-							color={colors.black}
-						/>
-						<Text style={styles.catText}>Category</Text>
-						<Text style={styles.catDynText}>Vehicle</Text>
+					<View>
+						<Text>Add Property</Text>
+						<Text>Menu</Text>
 					</View>
 				</View>
 			</View>
