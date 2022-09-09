cdb35769589545a9bb01889b1c59d3880f2c269e
Allen Dungo <allendungo@Allens-MBP.broadband>
2022-09-09 17:19:43 +0100

    Add Category Sidebar for Desktop Version

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -3,11 +3,13 @@ import PostItems from "../../components/postItems";
 import HeaderForMobile from "../../components/headerForMobile";
 import { getListingByCreatedAt } from "../../graphql/queries";
 import { API } from "aws-amplify";
-import { FlatList, View } from "react-native";
+import { FlatList, View, Text, Dimensions } from "react-native";
 import HeaderForDesktop from "../../components/headerForDesktop";
 import { colors } from "../../modal/color";
+import CategoryForDesktop from "../../components/categoryForDesktop";
 
 const Home = () => {
+	const windowWidth = Number(Dimensions.get("window").width);
 	const [newItems, setNewItems] = useState([]);
 	const fetchAll = async () => {
 		try {
@@ -27,14 +29,40 @@ const Home = () => {
 	});
 
 	return (
-		<View style={{ backgroundColor: colors.background }}>
+		<>
 			<HeaderForMobile />
 			<HeaderForDesktop />
-			<FlatList
-				data={newItems}
-				renderItem={({ item }) => <PostItems post={item} />}
-			/>
-		</View>
+			<View
+				style={{
+					flex: 1,
+					alignItems: "center",
+					backgroundColor: windowWidth > 800 ? colors.background : "",
+				}}
+			>
+				<View
+					style={{
+						flex: 1,
+						width: windowWidth > 800 ? "80%" : "100%",
+						flexDirection: "row",
+						justifyContent: "center",
+					}}
+				>
+					<View
+						style={{
+							display: windowWidth > 800 ? "flex" : "none",
+							flexBasis: "20%",
+						}}
+					>
+						<CategoryForDesktop />
+					</View>
+					<FlatList
+						style={{ flexBasis: "80%" }}
+						data={newItems}
+						renderItem={({ item }) => <PostItems post={item} />}
+					/>
+				</View>
+			</View>
+		</>
 	);
 };
 
