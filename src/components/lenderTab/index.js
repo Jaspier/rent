fe0995fab92cafa77b6c280a85b1b2245aee6b64
Allen Dungo <allendungo@Allens-MBP.broadband>
2022-09-12 17:54:26 +0100

    Completion of Lender Tab Screen

---
+++ src/components/lenderTab/index.js
@@ -0,0 +1,41 @@
+import { Text, View, Image, Pressable } from "react-native";
+import styles from "./styles";
+import { useNavigation } from "@react-navigation/native";
+import React, { useState } from "react";
+
+const LenderTabScreen = (props) => {
+	const navigation = useNavigation();
+	const post = props.LenderTab;
+	const [images, setImages] = useState(JSON.parse(post.listing.images));
+	return (
+		<View>
+			<Pressable
+				// onPress={() => {
+				// 	navigation.navigate("PostDetails", {
+				// 		postInfo: post,
+				// 	});
+				// }}
+				style={styles.container}
+			>
+				<View style={styles.postWrap}>
+					<Image
+						source={{
+							uri: `https://d24upkfrw5ljca.cloudfront.net/fit-in/400x400/public/${images[0].imageUri}`,
+						}}
+						style={styles.postImage}
+					></Image>
+					<View style={styles.postContentWrap}>
+						<View>
+							<Text style={styles.postTitle}>{post.listing.title}</Text>
+							<Text style={styles.postPlace}>{post.listing.locationName}</Text>
+						</View>
+						<Text style={styles.postValue}>
+							£{post.listing.rentValue} / day
+						</Text>
+					</View>
+				</View>
+			</Pressable>
+		</View>
+	);
+};
+export default LenderTabScreen;
--- src/screens/Lender/index.js
+++ src/screens/Lender/index.js
@@ -1,10 +1,47 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { API, Auth } from "aws-amplify";
+import React, { useState, useEffect } from "react";
+import { FlatList, View } from "react-native";
+import LenderTabScreen from "../../components/lenderTab";
+import { listRentOrders } from "../../graphql/queries";
 
 const LenderScreen = () => {
+	const [newItems, setNewItems] = useState([]);
+	const [userID, setUserID] = useState("");
+	Auth.currentAuthenticatedUser()
+		.then((user) => {
+			// console.log(user.attributes.sub);
+			setUserID(user.attributes.sub);
+		})
+		.catch((err) => {
+			console.log(err);
+			throw err;
+		});
+
+	const fetchAll = async () => {
+		try {
+			const orderList = await API.graphql({
+				query: listRentOrders,
+				variables: {
+					filter: {
+						lenderUserID: { eq: userID },
+					},
+				},
+				authMode: "AMAZON_COGNITO_USER_POOLS",
+			});
+			setNewItems(orderList.data.listRentOrders.items);
+		} catch (err) {
+			console.log(err);
+		}
+	};
+	useEffect(() => {
+		fetchAll();
+	});
 	return (
 		<View>
-			<Text>It's the Lender Screen</Text>
+			<FlatList
+				data={newItems}
+				renderItem={({ item }) => <LenderTabScreen LenderTab={item} />}
+			/>
 		</View>
 	);
 };
